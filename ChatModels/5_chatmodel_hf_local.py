4094d8cd8459753ccf72627968740ed4d439a576
Prakhar Maheshwari <prakharmaheshwari33@gmail.com>
2026-01-29 18:12:35 +0530

    code changed

--- ChatModels/5_chatmodel_hf_local.py
+++ ChatModels/5_chatmodel_hf_local.py
@@ -1,7 +1,8 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-import os
 
-os.environ["HF_HOME"] = "D:/huggingface_cache"
+# import os
+
+# os.environ["HF_HOME"] = "D:/huggingface_cache"
 
 llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
